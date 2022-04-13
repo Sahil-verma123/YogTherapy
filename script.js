c46daf70a1300818cdf05e10f003e26e884464a6
sahil verma <78171868+Sahil-verma123@users.noreply.github.com>
2022-04-13 22:48:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+// ****** NAVIGATION BAR ******
+
+const menu = document.querySelector(".menu");
+const navbar = document.querySelector(".navbar");
+
+menu.addEventListener("click", () => {
+  navbar.classList.toggle("change");
+  menu.classList.toggle('change')
+});
+
+// ****** End of NAVIGATION BAR *******
